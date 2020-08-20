b24b6f4e4d35faabda3cb610ae7af6b74b0fdbf2
Sergey Bubnov <serhii.bubnov@playtech.com>
2020-08-20 15:18:04 +0400

    nixify build

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ nixpkgs ? import <nixpkgs> {}, unstable ? import <unstable> {}, compiler ? "ghc8101" }:
+with nixpkgs;
+let
+  inherit (nixpkgs) pkgs;
+  myghc = unstable.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
+          cabal-install
+        ]);
+  libs = [ SDL2 zlib ];
+in
+pkgs.stdenv.mkDerivation {
+  name = "Haskan";
+  buildInputs = [ pkgconfig myghc ] ++ libs;
+  shellHook = ''
+    eval $(egrep ^export ${ghc}/bin/ghc)
+    LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${stdenv.lib.makeLibraryPath libs}"
+    echo "Haskan nix dev environment"
+  '';
+}
